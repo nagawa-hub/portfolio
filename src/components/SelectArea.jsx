f9b07b01532229d90fa3e47f6f6748f84d07162b
nagawa-hub <nakagawayu2008@gmail.com>
2020-11-16 10:35:31 +0900

    ボタン表示

--- src/components/SelectArea.jsx
+++ src/components/SelectArea.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Select} from './index';
 
-const SelectArea = () => {
+const SelectArea = (props) => {
   return(
     <div className="select_area">
-
+      <Select text={"自己紹介"} />
+      <Select text={"スキル"} />
+      <Select text={"制作アプリ"} />
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export {default as Header} from './Header'
 export {default as SelectArea} from './SelectArea'
 export {default as MainContents} from './MainContents'
+
+export {default as Select} from './Select'
