656ccce027172092ad4c63550f402e67fcf8d167
nagawa-hub <nakagawayu2008@gmail.com>
2020-12-07 14:09:07 +0900

    アプリ説明を追加

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -8,6 +8,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Sarabred from '../assets/img/sarabred.png';
 import Furima from '../assets/img/furima.png';
+import Bookshelf from '../assets/img/bookshelf.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,6 +22,10 @@ const useStyles = makeStyles((theme) => ({
     height: 0,
     paddingTop: '56.25%', // 16:9
   },
+  mobile:{
+    height: 130,
+    paddingTop: '78%',
+  }
 }));
 
 const Create = () => {
@@ -80,6 +85,35 @@ const Create = () => {
           アプリへはこちらから
         </Button>
       </div>
+
+      <div className={classes.content}>
+        <CardHeader
+        title="本のレビューアプリ"
+        subheader="本の共有・レビュー"
+      />
+      <CardMedia
+        className={classes.mobile}
+        image={Bookshelf}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          読んだ本の共有、レビューができるモバイルアプリです。<br/>
+          本の写真投稿と、投稿に対してコメントが可能です。<br/>
+          またユーザー認証に関してはFirebaseの匿名ログインを使っています。<br/>
+          言語：<span className="react_tag">React Native</span><br/>
+          DB: <span className="firebase_tag">Firebase</span>
+        </Typography>
+      </CardContent>
+        <Button 
+          variant="outlined" 
+          color="primary"
+          target="_blank"
+          href="https://github.com/nagawa-hub/bookshelf"
+        >
+          Githubへはこちらから
+        </Button>
+      </div>
+    
     </Card>    
   )
 }
